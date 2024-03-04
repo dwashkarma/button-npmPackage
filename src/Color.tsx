965a97aca4570b9e9925fbe456087bc497c1fbe9
DwashKarma <dwaskc@gmail.com>
2024-03-04 13:24:55 +0545

    refactor:text color

--- src/Color.tsx
+++ src/Color.tsx
@@ -1,16 +1,16 @@
 export const colorVariant = (bgColor: string | undefined) => {
   switch (bgColor) {
     case "primary":
-      return { backgroundColor: "#3B71CA", color: "black" };
+      return { backgroundColor: "#3B71CA", color: "332D2D" };
 
     case "secondary":
-      return { backgroundColor: "#9FA6B2", color: "black" };
+      return { backgroundColor: "#9FA6B2", color: "332D2D" };
     case "success":
       return { backgroundColor: "green", color: "white" };
     case "danger":
       return { backgroundColor: "#e74c3c", color: "white" };
 
     default:
-      return { backgroundColor: "transparent", color: "black" };
+      return { backgroundColor: "transparent", color: "332D2D" };
   }
 };
