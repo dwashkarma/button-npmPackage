020795dc01ec175a89c5e6168b0819c4b5a54f1c
DwashKarma <dwaskc@gmail.com>
2024-03-04 12:56:59 +0545

    refactor: added rollup and updated package.json

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+// rollup.config.js
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
+  ],
+  plugins: [typescript(), terser()],
+};
